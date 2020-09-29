e727aef4b0448f6f58152e12aa5d5ef26a2f9fa9
Luke Schissler <lukeschissler@gmail.com>
2020-09-29 12:17:03 -0400

    Comments, animations, pre route switching

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 // MongoDB/App
 MongoClient.connect(connectionString, {
--- public/javascripts/directions.js
+++ public/javascripts/directions.js
@@ -2,12 +2,275 @@
 // parameter when you first load the API. For example:
 // <script
 // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+var map;
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
+
+    // Create a new StyledMapType object, passing it an array of styles,
+    // and the name to be displayed on the map type control.
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#ebe3cd"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#523735"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#f5f1e6"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#c9b2a6"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#dcd2be"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#ae9e90"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dfd2ae"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dfd2ae"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#93817c"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#a5b076"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#447530"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#f5f1e6"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#fdfcf8"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#f8c967"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#e9bc62"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#e98d58"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway.controlled_access",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#db8555"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#806b63"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dfd2ae"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#8f7d77"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#ebe3cd"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#dfd2ae"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#b9d3c2"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#92998d"
+                    }
+                ]
+            }
+        ],
+        {name: 'Styled Map'});
+
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
+    var map = new google.maps.Map(document.getElementById('map'), {
         mapTypeControl: false,
-        center: { lat: 42.35, lng: -71.08 },
-        zoom: 12
+        center: {lat: 42.3601, lng: -71.0589},
+        zoom: 11,
     });
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
     new AutocompleteDirectionsHandler(map);
     const bikeLayer = new google.maps.BicyclingLayer();
 }
@@ -19,7 +282,12 @@ class AutocompleteDirectionsHandler {
         this.destinationPlaceId = "";
         this.travelMode = google.maps.TravelMode.BICYCLING;
         this.directionsService = new google.maps.DirectionsService();
-        this.directionsRenderer = new google.maps.DirectionsRenderer();
+        this.directionsRenderer = new google.maps.DirectionsRenderer({polylineOptions: {
+                strokeColor: "#A95182",
+                strokeWeight: "5",
+                strokeOpacity: "0.8"
+            }
+        });
         this.directionsRenderer.setPanel(document.getElementById("right-panel"));
         this.directionsRenderer.setMap(map);
         const originInput = document.getElementById("origin-input");
@@ -60,16 +328,53 @@ class AutocompleteDirectionsHandler {
                 this.destinationPlaceId = place.place_id;
             }
             this.route();
-            this.existingRoute();
         });
     }
+    showComments() {
+        const stuff = document.getElementById("stuff-panel");
+
+        stuff.id = "stuff-panel-visible";
+
+        let activeComment;
+        const allComments =  document.getElementsByClassName("comment");
+
+        Array.from(allComments).forEach(comment => {
+            comment.addEventListener('click', event => {
+                if (!activeComment) {
+                    activeComment = comment;
+                    comment.style['background-color'] = 'rgba(109, 104, 117, 0.2)';
+                    this.existingRoute();
+                    this.scrollUp();
+                } else if (activeComment !== comment) {
+                    activeComment.style['background-color'] = 'rgba(242, 233, 228, 0.3)';
+                    activeComment = comment;
+                    comment.style['background-color'] = 'rgba(109, 104, 117, 0.2)';
+                    this.existingRoute();
+                    this.scrollUp();
+                }
+            })
+        })
+    }
+
+    scrollDown() {
+        window.scrollBy(0, 100);
+    }
+
+    scrollUp() {
+        window.scrollBy(0, -1000000)
+    }
+
     existingRoute() {
         const me = this;
         const directionsRenderer = new google.maps.DirectionsRenderer({polylineOptions: {
-                strokeColor: "red"
+                strokeColor: "#7851a9",
+                strokeWeight: "5",
+                strokeOpacity: "0.8"
             }
         });
+
         directionsRenderer.setMap(this.map);
+
         this.directionsService.route(
             {
                 origin: "Boston, MA" ,
@@ -84,6 +389,7 @@ class AutocompleteDirectionsHandler {
                 }
             }
         );
+
     }
 
     route() {
@@ -91,6 +397,10 @@ class AutocompleteDirectionsHandler {
             return;
         }
         const me = this;
+        document.getElementById('origin-hidden').innerText = this.originPlaceId
+        document.getElementById('dest-hidden').innerText = this.destinationPlaceId
+
+
         this.directionsService.route(
             {
                 origin: { placeId: this.originPlaceId },
@@ -99,7 +409,17 @@ class AutocompleteDirectionsHandler {
             },
             (response, status) => {
                 if (status === "OK") {
+                    //set the directions to the answer of the query
                     me.directionsRenderer.setDirections(response);
+
+                    //show the directions panel and the comments panel
+                    const dirPanel = document.getElementById('dir-panel');
+                    dirPanel.id = 'dir-panel-visible';
+                    this.showComments();
+
+                    //scroll down to the comments
+                    this.scrollDown();
+
                 } else {
                     window.alert("Directions request failed due to " + status);
                 }
@@ -107,3 +427,40 @@ class AutocompleteDirectionsHandler {
         );
     }
 }
+
+
+function formSubmit(event){
+    //Prevent form from firing
+    event.preventDefault()
+
+    //Get all the form data
+    const formID = document.getElementById('id-input').value
+    const formDescription = document.getElementById('desc-input').value
+    const formRating = document.getElementById('rating-input').value
+    const origin = document.getElementById('origin-hidden').innerHTML
+    const destination = document.getElementById('dest-hidden').innerHTML
+
+    //Do validation ...
+
+    //Send post request via fetch
+    const commentData = {
+        id : formID,
+        description : formDescription,
+        rating : formRating,
+        origin : origin,
+        destination : destination
+    }
+
+    fetch("/", {
+        method: "post",
+        headers: { "Content-Type": 'application/json' },
+        body: JSON.stringify(commentData)
+    }).then(res => {
+        console.log("request complete! Response:", res);
+    });
+}
+
+
+
+
+
